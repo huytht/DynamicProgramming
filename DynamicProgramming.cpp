8ac5731027ddf0a43d01aa6db9bf975381840614
huytht <81338938+huytht@users.noreply.github.com>
2021-06-26 12:35:30 +0700

    Update DynamicProgramming.cpp

--- DynamicProgramming.cpp
+++ DynamicProgramming.cpp
@@ -1,72 +1,69 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[100],W,c[100],f[100][100];
-int n,i,j,GT;
+int W[100], V[100], f[100][100];
+int N, i, j, GT, M;
 
 void nhap(){
-	cout<<"nhap n: ";
-	cin>>n;
-	cout<<"nhap W: ";
-	cin>>W;
-	for(i=1;i<=n;i++){
-		cout<<"nhap khoi luong doi vat thu " << i<< " ";
-		cin>>a[i];
-	}
-	for(i=1;i<=n;i++){
-		cout<<"nhap gia tri doi vat thu " << i << " ";
-		cin>>c[i];
+	cout<<"nhap N: ";
+	cin>>N;
+	cout<<"nhap M: ";
+	cin>>M;
+	for(i = 1; i <= N; i++){
+		cout << "nhap khoi luong mon hang thu " << i << ": ";
+		cin >> W[i];
+		cout << "nhap gia tri mon hang thu " << i << ": ";
+		cin >> V[i];
 	}
 }
 void xuat(){
-	cout<<"\n\n---bang---\n";
-	for(i = 1; i <= n; i++){
-		for(j = 0; j <= W; j++){
+	cout << "\n\n---bang---\n";
+	for(i = 1; i <= N; i++){
+		for(j = 0; j <= M; j++){
 			cout << f[i][j];
 		}
 		cout << "\n";
 	}
 }
-int max(int a,int b){
+int max(int a, int b){
 	return (a > b) ? a : b;
 }
 int caiTui(){
-	for(i=0;i<=n;i++){
-		f[i][0]=0;
+	for(i = 0; i <= N; i++){
+		f[i][0] = 0;
 	}
-	for(j=0;j<=W;j++){
-		f[0][j]=0;	
+	for(j = 0; j <= M; j++){
+		f[0][j] = 0;	
 	}
-	for(i=1;i<=n;i++){
-		for(j=1;j<=W;j++){
-			if(a[i]<=j){
-				f[i][j]=max(f[i-1][j],f[i-1][j-a[i]]+c[i]);
+	for(i = 1; i <= N; i++){
+		for(j = 1; j <= M; j++){
+			if(W[i] <= j){
+				f[i][j] = max(f[i-1][j], f[i-1][j-W[i]] + V[i]);
 			}
 			else{
-				f[i][j]=f[i-1][j];
+				f[i][j] = f[i-1][j];
 			}
 		}
 	}
 }
 int luuVet(){
-	i=n;
-	j=W;
-	while((i!=0)&&(j!=0)){
-		if(f[i][j]!=f[i-1][j]){
-			cout<<i<<" ";
-			GT+=c[i];
-			j-=a[i];
+	i = N;
+	j = M;
+	while((i != 0) && (j != 0)){
+		if(f[i][j] != f[i - 1][j]){
+			cout << i << " ";
+			GT += V[i];
+			j -= W[i];
 		}
 		i--;
 	}
 }
 int main(){
 	nhap();
-	cout<<"\n------cac gia tri sau khi nhap------";
+	cout << "\n------cac gia tri sau khi nhap------";
 	caiTui();
-	cout<<"\n cac do vat duoc chon: ";
+	cout << "\n cac mon hang duoc chon: ";
 	luuVet();
-	cout<<"\n\n tong gia tri cua do vat cho vao tui la: "<<GT<<" ";
+	cout << "\n\n tong gia tri cua cac mon hang cho vao tui la: " << GT << " ";
 
 	return 0;
 }
-
